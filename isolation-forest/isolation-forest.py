b8052a25684fea6e889a052b0c7461087c49c53f
waasiq <waasiqmasood@gmail.com>
2022-01-28 11:16:13 +0300

    Updated Isolation Forest Algorithm
    Updated Isolation Forest Algorithm for test sets and the model to use contamination.

--- isolation-forest/isolation-forest.py
+++ isolation-forest/isolation-forest.py
@@ -17,52 +17,70 @@ dataset = pd.read_csv('./Water_Data.csv')
 # Convert the string to pandas datetime
 dataset['timestamp'] = pd.to_datetime(dataset['timestamp']) 
 
-# Putting the Data in Missing 
+# Filling the missing NaN data in Missing data pad = propagate last valid observation to next value
 dataset = dataset.fillna(method = 'pad')
 
 
+# Taking  two sub data sets both for graph plotting and sub data
 sub_dataset = dataset.filter(['timestamp','Turb(FNU)'], axis=1)
-t = sub_dataset
+graph_plotter = sub_dataset
 
 X = dataset.iloc[:,:1].values
 y = dataset.iloc[:,2:3].values
 
+#--------------------------- Start of test data split ----------------------
 
+'''
 # Splitting the dataset into the training set and data set
+# And preparing the frames for the test data  
 
-#from sklearn.model_selection import train_test_split
-#X_train, X_test , y_train, y_test = train_test_split(X,y, test_size = 0.2, random_state = 0)
+from sklearn.model_selection import train_test_split
+X_train, X_test , y_train, y_test = train_test_split(X,y, test_size = 0.2, random_state = 0)
+X_test = X_test.astype('float64')
 
+sub_dataset = pd.DataFrame(np.hstack((X_test,y_test))) #Convert to dataframe
+# Renaming the columns from 0,1 to timestamp and placeholder
+sub_dataset.columns = { 'Turb(FNU)' , 'placeholder'}
+sub_dataset.rename(columns = {'placeholder' : 'timestamp'}, inplace = True)
+sub_dataset['timestamp'] = pd.to_datetime(sub_dataset['timestamp'])
+graph_plotter = sub_dataset 
 
-# Fitting the regression model to the dataset
+'''
+#--------------------------- End of Test Data Split -----------------------
+
+# Fitting the IsolationForest model to the dataset
+# If used for test model change the X to X_train in fitting 
+# While cahnge the predict to the X_train and decision_function
 from sklearn.ensemble import IsolationForest
-regressor = IsolationForest(contamination = 'auto')
-regressor.fit(X)
+model = IsolationForest(n_estimators = 100, max_samples = 'auto', contamination = 0.4, random_state = 42)
+model.fit(X, y)
 
 # Predicting the result with Isolation Forest Method
-anamoly = regressor.predict(X)
-anomaly_score  = regressor.decision_function(X)
+anamoly = model.predict(X)
+anomaly_score  = model.decision_function(X)
 
 
-# ----------------------- ALGORITHM FINSHES ----------------------------
+# ----------------------- Graph Visualization Below -----------------------
+
 
 # Visualizing the Isolation Forest Results
-t['anomaly']=pd.Series(regressor.predict(X))
-anomaly_data = t.loc[t['anomaly'] == -1, ['timestamp','Turb(FNU)']]
+graph_plotter['anomaly']= pd.Series(anamoly)
+anomaly_data = graph_plotter.loc[graph_plotter['anomaly'] == -1, ['timestamp','Turb(FNU)']]
 
-t = t.fillna(method = 'pad')
+# Filling the NaN data in both the data frames
+graph_plotter = graph_plotter.fillna(method = 'pad')
 anomaly_data = anomaly_data.fillna(method = 'pad')
 
-#print("Percentage of anomalies in data: {:.2f}".format((len(t.loc[t['anomaly']==-1])/len(t))*100))
 
+# Prints the percentage anomalies in data
+print("Accuracy:", list(anamoly).count(1)/anamoly.shape[0])
+print("Percentage of anomalies in data: {:.2f}".format((len(graph_plotter.loc[graph_plotter['anomaly']==-1])/len(graph_plotter))*100))
 
 plt.style.use('seaborn')
-plt.figure(figsize=(12,8))
+plt.figure(figsize=(12,8)) # Set the figure size
 
-plt.plot(t['timestamp'],t['Turb(FNU)'], color='blue', label = 'Normal')
+plt.plot(graph_plotter['timestamp'],graph_plotter['Turb(FNU)'], color='blue', label = 'Normal')
 plt.plot_date(x=anomaly_data['timestamp'],y=anomaly_data['Turb(FNU)'], color='red', label = 'Anomaly')
-
-#plt.plot(predictions, label='Predictions')
 
 plt.gcf().autofmt_xdate()
 date_format = mpl_dates.DateFormatter('%d, %b, %Y')
@@ -73,4 +91,3 @@ plt.ylabel("Turbidity Values(FNU)")
 plt.title('Anomaly Detection : Isolation Forest Method')
 plt.legend()
 plt.show()
-
