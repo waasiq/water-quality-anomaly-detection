c88243a8433cd9e875a5551aa82a773f999e4eb7
Yusuf Ozaslan <74647471+YusufOzaslan@users.noreply.github.com>
2022-01-31 16:28:11 +0300

    Update logistic-regression.py

--- logistic-regression/logistic-regression.py
+++ logistic-regression/logistic-regression.py
@@ -11,7 +11,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 from matplotlib import dates as mpl_dates
 # Data upload
-dataset = pd.read_csv('../data/water-data-log-reg.csv')
+dataset = pd.read_csv('..\Doc\Water_Data.csv')
 
 dataset['timestamp'] = pd.to_datetime(dataset['timestamp']) 
 
@@ -27,7 +27,7 @@ missingValues[:,0:3] = imputer.transform(missingValues[:,0:3])
 print(missingValues)
 
 #------------------------------------------
-outcome = pd.DataFrame(data=missingValues, index=range(99), columns=["SC(uS)", "Turb(FNU)", "DO(mg/L)"])
+outcome = pd.DataFrame(data=missingValues, index=range(5171), columns=["SC(uS)", "Turb(FNU)", "DO(mg/L)"])
 print(outcome)
 
 outcomeConcat = pd.concat([time, outcome], axis = 1)
@@ -39,14 +39,22 @@ y = outcomeConcat.iloc[:,0:1]
 X = x.values
 Y = y.values
 
-# Logistic Regression
+from sklearn.preprocessing import StandardScaler
+sc=StandardScaler()
+X = sc.fit_transform(X)
 
+# Logistic Regression
 from sklearn.linear_model import LogisticRegression
 logr = LogisticRegression(random_state=0)
 logr.fit(X,Y)
 
 Y_pred = logr.predict(X)
 
+print(Y_pred)
+anomaly_score  = logr.decision_function(X)
+#quality = pd.DataFrame(data=Y_pred, index=range(99), columns=["Quality"])
+
+# quality ekrana yazdır Plot bak
 # ----------------------- Graph Visualization Below -----------------------
 # Visualizing the Logistic Regression Results
 outcomeConcat['anomaly']= pd.Series(Y_pred)
@@ -55,12 +63,13 @@ outcomeConcat['anomaly']= pd.Series(Y_pred)
 plt.style.use('seaborn')
 plt.figure(figsize=(12,8)) # Set the figure size
 
-plt.plot(outcomeConcat['timestamp'],outcomeConcat['Turb(FNU)'], color='blue', label = 'Normal')
+
+plt.plot(outcomeConcat['timestamp'],outcomeConcat['anomaly'], color='blue', label = 'Normal')
 #plt.plot(outcomeConcat['timestamp'],outcomeConcat['SC(uS)'], color='green', label = 'Normal')
 #plt.plot(outcomeConcat['timestamp'],outcomeConcat['DO(mg/L)'], color='black', label = 'Normal')
-#plt.plot_date(x=anomaly_data['timestamp'],y=anomaly_data['Turb(FNU)'], color='red', label = 'Anomaly Turb(FNU)')
-#plt.plot_date(x=anomaly_data['timestamp'],y=anomaly_data['SC(uS)'], color='green', label = 'Anomaly SC(uS)')
-#plt.plot_date(x=anomaly_data['timestamp'],y=anomaly_data['DO(mg/L)'], color='black', label = 'Anomaly DO(mg/L)')
+#plt.scatter(outcomeConcat['timestamp'],outcomeConcat['Turb(FNU)'],color='red', label = 'Anomaly Turb(FNU)')
+plt.scatter(outcomeConcat['timestamp'],outcomeConcat['anomaly'],color='black', label = 'Anomaly DO(mg/L)')
+#plt.scatter(outcomeConcat['timestamp'],outcomeConcat['SC(uS)'],color='green', label = 'Anomaly SC(uS)')
 
 plt.gcf().autofmt_xdate()
 date_format = mpl_dates.DateFormatter('%d, %b, %Y')
